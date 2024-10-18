1a8de670e55e6b0ee3f8135564ce75a8123cb5d7
Landung 'Don' Setiawan <landungs@uw.edu>
2024-10-17 17:21:47 -0700

    chore: Restructure code to prepare for Rust + Python

---
+++ crates/phylo2vec/src/main.rs
@@ -0,0 +1,8 @@
+mod to_newick;
+
+fn main() {
+    // Currently a small testing routine to ensure things are working as intended
+    let v = vec![0,2,2,5,2];
+    let newick_string = to_newick::to_newick(v);
+    print!("{}", newick_string);
+}
