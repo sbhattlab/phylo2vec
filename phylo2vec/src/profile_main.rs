3ae7bb36d92b3756c6014cff957150599f07ce48
Ayush Nag <35325113+ayushnag@users.noreply.github.com>
2025-04-18 16:28:26 -0700

    chore: Fix profiling imports for Rust CI (#126)
    * chore: update profiling imports for CI
    
    * refactor: switch to to_newick_from_vector

--- phylo2vec/src/profile_main.rs
+++ phylo2vec/src/profile_main.rs
@@ -1,5 +1,5 @@
 use phylo2vec::{
-    tree_vec::ops::{to_newick, to_vector},
+    tree_vec::ops::{to_newick_from_vector, to_vector},
     utils::sample_vector,
 };
 use std::env;
@@ -14,7 +14,7 @@ fn main() {
         DEFAULT_N_LEAVES
     };
     let v = sample_vector(n_leaves, true);
-    let n = to_newick(&v);
+    let n = to_newick_from_vector(&v);
     let re_v = to_vector(&n);
     println!("vector length: {:?}", re_v.len());
 }
