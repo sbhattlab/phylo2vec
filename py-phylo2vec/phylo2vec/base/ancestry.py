f06409588223763adcaa2baa5b894b266150d0ec
Neclow <neilclow24@gmail.com>
2025-05-01 13:48:49 +0000

    docs: prettify docs for to_ancestry

--- py-phylo2vec/phylo2vec/base/ancestry.py
+++ py-phylo2vec/phylo2vec/base/ancestry.py
@@ -30,21 +30,23 @@ def from_ancestry(ancestry: np.ndarray) -> np.ndarray:
 def to_ancestry(v: np.ndarray) -> np.ndarray:
     """Convert a Phylo2Vec vector to an ancestry matrix
 
-    v[i] = which BRANCH we do the pairing from
+    v[i] indicates which branch we do the pairing from.
 
     The initial situation looks like this:
-                      R
-                      |
-                      | --> branch 2
-                    // \\
-      branch 0 <-- //   \\  --> branch 1
-                   0     1
 
-    For v[1], we have 3 possible branches too choose from.
-    v[1] = 0 or 1 indicates that we branch out from branch 0 or 1, respectively.
-    The new branch yields leaf 2 (like in ordered trees)
+    0 and 1 are leaves, which form a cherry.
+    2 is the parent node of 0 and 1.
+    R is an "extra root" that is connected to 2.
+    In terms of edges:
+    0 -- 2 (branch 0)
+    1 -- 2 (branch 1)
+    2 -- R (branch 2)
 
-    v[1] = 2 is somewhat similar: we create a new branch from R that yields leaf 2
+    For v[1], we have 3 possible branches to choose from.
+    v[1] = 0 or 1 indicates that we branch out from branch 0 or branch 1, respectively.
+    The new branch yields leaf 2 (like in ordered trees).
+
+    v[1] = 2 is somewhat similar: we create a new branch from R that yields leaf 2.
 
     Parameters
     ----------
@@ -59,5 +61,6 @@ def to_ancestry(v: np.ndarray) -> np.ndarray:
         2nd column: child 2
         3rd column: parent node
     """
+
     ancestry_list = core.get_ancestry(v)
     return np.asarray(ancestry_list)
